3678b617b93571f7a0a656b195bfe3fbe31e834e
Alyson La <2623954+alysonla@users.noreply.github.com>
2025-04-16 00:22:09 -0600

    fix data

--- src/data/githubLanguageData.ts
+++ src/data/githubLanguageData.ts
@@ -3,27 +3,27 @@ export const githubLanguageData = [
   {
     name: 'Python',
     color: '#3EAFA8',
-    values: [4, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1]
+    values: [4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 1]
   },
   {
     name: 'JavaScript',
     color: '#DD78E8',
-    values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]
+    values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]
   },
   {
     name: 'TypeScript',
     color: '#E882B5',
-    values: [8, 7, 6, 6, 7, 5, 3, 3, 3, 3, 3]
+    values: [null, null, null, null, null, 4, 4, 4, 4, 3, 3]
   },
   {
     name: 'Java',
     color: '#6F8FF7',
-    values: [2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4]
+    values: [2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4]
   },
   {
     name: 'C#',
     color: '#B896EF',
-    values: [5, 5, 6, 7, 6, 7, 5, 5, 5, 5, 5]
+    values: [8, 7, 6, 6, 7, 7, 5, 5, 5, 5, 5]
   },
   {
     name: 'C++',
@@ -33,17 +33,17 @@ export const githubLanguageData = [
   {
     name: 'PHP',
     color: '#5DAAF8',
-    values: [3, 4, 4, 4, 4, 4, 6, 6, 8, 9, 7]
+    values: [3, 4, 4, 4, 4, 5, 6, 6, 7, 7, 7]
   },
   {
     name: 'Shell',
     color: '#7CA2F7',
-    values: [9, 8, 8, 9, 8, 8, 8, 8, 7, 8, 8]
+    values: [9, 9, 9, 9, 9, 9, 9, 8, 8, 9, 8]
   },
   {
     name: 'C',
     color: '#E88DCA',
-    values: [7, 9, 9, 8, 9, 9, 9, 9, 9, 8, 9]
+    values: [7, 8, 8, 7, 8, 8, 8, 9, 9, 8, 9]
   },
   {
     name: 'Go',
@@ -53,7 +53,7 @@ export const githubLanguageData = [
   {
     name: 'Ruby',
     color: '#D4A675',
-    values: [8, 8, 7, 10, 10, 10, 10, 10, 10, null, null]
+    values: [5, 5, 7, 10, 10, 10, 10, 10, 10, null, null]
   },
   {
     name: 'Obj-C',
